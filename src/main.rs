c9eb229b01b422b88d7e4fb8640a01da188eba1c
Jeevitha Kannan K S <ksjeevithakannan123@gmail.com>
2025-02-13 10:24:13 +0530

    added basic input box and display the msg
    Emojis cannot be used due to the usage of string and not unicode width

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use ratatui::{
+    crossterm::event::{self, Event, KeyEventKind},
+    // prelude::*,
+    DefaultTerminal,
+};
+use std::io::Result;
+
+mod app;
+use app::App;
+
+fn main() -> Result<()> {
+    let mut terminal = ratatui::init();
+    let mut app = App::new();
+    let app_result = run(&mut terminal, &mut app);
+    ratatui::restore();
+    app_result
+}
+
+fn run(terminal: &mut DefaultTerminal, app: &mut App) -> Result<()> {
+    let mut should_exit = false;
+    while !should_exit {
+        terminal.draw(|frame| app.draw(frame))?;
+        if let Event::Key(key) = event::read()? {
+            if key.kind != KeyEventKind::Press && key.kind != KeyEventKind::Repeat {
+                continue;
+            }
+
+            should_exit = app.on_key(key);
+        }
+    }
+    Ok(())
 }
